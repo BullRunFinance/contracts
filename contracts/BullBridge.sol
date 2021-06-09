e02b4d4aa911140e2bf671c630407a8f6cf3f5ae
BullRunFinance <socials@bullrun.finance>
2021-06-08 23:05:10 -0300

    bridge balances control moved from contract to router

--- contracts/BullBridge.sol
+++ contracts/BullBridge.sol
@@ -19,14 +19,12 @@ contract BullBridge is Ownable, Pausable {
         address to;
         uint256 amount;
         uint256 chainId;
-        bool completed;
     }
 
     struct Token {
         IBEP20 token;
         uint256 minTransfer;
         mapping(uint256 => bool) enabledChains;
-        mapping(uint256 => uint256) bridgeBalances;
     }
 
     address public operator;
@@ -42,10 +40,7 @@ contract BullBridge is Ownable, Pausable {
     // History of bull token transfered by the user in the bridge
     mapping(address => uint256) bullTransfered;
 
-
-    event UpdatedBalance(uint8 tokenId, uint256 amount);
     event CreatedCrossTransfer(uint256 orderId, uint8 tokenId, address indexed from, address indexed to, uint256 amount, uint256 chainId);
-    event CompletedOrder(uint256 chainIdOrigin, uint256 orderId);
 
     modifier onlyOperator() {
         require(operator == msg.sender, "operator: caller is not the operator");
@@ -68,28 +63,22 @@ contract BullBridge is Ownable, Pausable {
     }
 
     function addLiquidity(uint8 _tokenId, uint256 _amount) external onlyOwner{
-        require(tokens[_tokenId].token.balanceOf(msg.sender) >= _amount, "Not enough balance");
         tokens[_tokenId].token.safeTransferFrom(msg.sender, address(this), _amount);
-        emit UpdatedBalance(_tokenId, tokens[_tokenId].token.balanceOf(address(this)));
     }
 
-    function addToken(uint8 _id, IBEP20 _token, uint256 _minTransfer, uint256[] memory _chains, uint256[] memory _balances) external onlyOwner nonReentrant(_id){
-        require(_chains.length == _balances.length, "Amount of chains and balances should be the same");
-
+    function addToken(uint8 _id, IBEP20 _token, uint256 _minTransfer, uint256[] memory _chains) external onlyOwner nonReentrant(_id){
         tokens[_id].token = _token;
         tokens[_id].minTransfer = _minTransfer;
         uint256 amountOfChains = _chains.length;
 
         for(uint256 x = 0; x < amountOfChains; x++){
             tokens[_id].enabledChains[_chains[x]] = true;
-            tokens[_id].bridgeBalances[_chains[x]] = _balances[x];
         }
     }
 
     function createCrossTransfer(uint8 _tokenId, address _to, uint256 _amount, uint256 _chainId) external whenNotPaused{
         require(address(tokens[_tokenId].token) != address(0), "Token not supported");
         require(tokens[_tokenId].enabledChains[_chainId], "Destination chain not supported");
-        require(tokens[_tokenId].bridgeBalances[_chainId] >= _amount, "Insufficient liquidity in destination chain");
 
         if(!bullNFT.hasBoost(msg.sender, theBigBull)){
             uint256 feeAmount = _amount.mul(fee).div(10000); 
@@ -98,7 +87,6 @@ contract BullBridge is Ownable, Pausable {
         }
 
         tokens[_tokenId].token.safeTransferFrom(msg.sender, address(this), _amount);
-        emit UpdatedBalance(_tokenId, tokens[_tokenId].token.balanceOf(address(this)));
 
         orders[nextOrderId] = Order (
             nextOrderId,
@@ -106,8 +94,7 @@ contract BullBridge is Ownable, Pausable {
             msg.sender,
             _to,
             _amount,
-            _chainId,
-            false
+            _chainId
         );
 
         emit CreatedCrossTransfer(nextOrderId, _tokenId, msg.sender, _to, _amount, _chainId);
@@ -127,26 +114,7 @@ contract BullBridge is Ownable, Pausable {
 
     function completeCrossTransfer(uint8 _tokenId, address _to, uint256 _amount, uint256 _originChain, uint256 _orderId) external onlyOperator whenNotPaused{
         require(tokens[_tokenId].token.balanceOf(address(this)) >= _amount, "Insufficient balance");
-
         tokens[_tokenId].token.safeTransfer(_to, _amount);
-
-        emit UpdatedBalance(_tokenId, tokens[_tokenId].token.balanceOf(address(this)));
-        emit CompletedOrder(_originChain, _orderId);
-    }
-
-    function setCompleteOrder(uint256 _id) external onlyOperator whenNotPaused {
-        require(orders[_id].completed == false, "Order already completed");
-        require(_id < nextOrderId, "Invalid id");
-        orders[_id].completed = true;
-    }
-
-    function updateBridgeTokenBalance(uint256 _chainId, uint8 _tokenId, uint256 _amount) external onlyOperator {
-        tokens[_tokenId].bridgeBalances[_chainId] = _amount;
-    }
-
-    function updatefee(uint16 _fee) external onlyOwner{
-        require(_fee <= 1000, "fee can't be more than 10%");
-        fee = _fee;
     }
 
     function pause() external onlyOwnerOrOperator whenNotPaused {
@@ -157,6 +125,11 @@ contract BullBridge is Ownable, Pausable {
 
     function unpause() external onlyOwner whenPaused {
         _unpause();
+    }
+
+    function updatefee(uint16 _fee) external onlyOwner{
+        require(_fee <= 1000, "fee can't be more than 10%");
+        fee = _fee;
     }
 
     function allowChain(uint8 _tokenId, uint256 _chainId) external onlyOwner{
